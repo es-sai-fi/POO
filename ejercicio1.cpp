a900ceef39781d9858406e8ea9f76028a5f7a84f
es-sai-fi <jhormangomez2005@outlook.com>
2023-09-08 20:27:43 -0500

    Se arreglaron errores en el ejercicio 4.

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -18,7 +18,7 @@ int tablasDeMultiplicar(int i, int j){ //Función que toma i, j y en base a ella
 }
 
 int main(){
-  cout << "  0 1 2 3 4 5 \n"; 
+  cout << "0 1 2 3 4 5\n"; 
   for (int i = 0; i <= 5; i++) { //Ciclo anidado, i es la tabla a sacar y j el número por el cual se multiplica.
     cout << i << " 0 ";
     for (int j = 1; j <= 5; j++) {
